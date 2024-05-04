acedd7c6b925fc14e4431d133aed66e53168be28
ahmedishraq <ishraqahmedbd@gmail.com>
2024-05-04 16:47:22 +0600

    added mineBlock() method to mining block

--- src/main/java/org/example/Block.java
+++ src/main/java/org/example/Block.java
@@ -4,27 +4,38 @@ import org.xml.sax.helpers.AttributesImpl;
 
 import java.util.Date;
 
-public class Block {
+public class Block{
     public String hash;
     public String previousHash;
-    private String data; // our data will be a simple message
-    private long timestamp; // as number of miliseconds since 1/1/1970
+    private String data; //our data will be a simple message.
+    private long timeStamp; // as number of milliseconds since 1/1/1970
+    private int nonce;
 
-
-    // Block Constructor.
+    //Block Constructor.
     public Block(String data, String previousHash){
         this.data = data;
         this.previousHash = previousHash;
-        this.timestamp = new Date().getTime();
-        this.hash = calculateHash(); //Making sure we do this after we set the other values.
+        this.timeStamp = new Date().getTime();
+
+        this.hash = calculateHash(); // Making sure we do this after we set the other values.
     }
 
+    //Calculate new hash based on blocks contents
     public String calculateHash(){
         String calculatedhash = StringUtil.applySha256(
                 previousHash +
-                        Long.toString(timestamp) +
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
                         data
         );
         return calculatedhash;
+    }
+    public void mineBlock(int difficulty){
+        String target = new String(new char[difficulty]).replace('\0', '0'); // create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)){
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!!! : "+ hash);
     }
 }
